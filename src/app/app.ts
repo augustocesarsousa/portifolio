3d46c705e85006f1f4199c136d6de592ab700a66
Augusto Cesar <acs-03@hotmail.com>
2026-03-23 06:23:26 -0300

    adiciona hero component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
+import { HeroComponent } from './features/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent],
   template: `
     <app-header />
+    <app-hero />
     <router-outlet />
     <p>Projeto em construção</p>
   `,
---
+++ src/app/features/hero/hero.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  templateUrl: './hero.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+})
+export class HeroComponent {
+
+}
