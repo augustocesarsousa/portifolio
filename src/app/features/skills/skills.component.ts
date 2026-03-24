a773a8ce6cefd3e0cd5ba6297904f5fb2cb0bd31
Augusto Cesar <acs-03@hotmail.com>
2026-03-24 05:22:49 -0300

    adiciona skills component

---
+++ src/app/features/skills/skills.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  imports: [],
+  templateUrl: './skills.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+})
+export class SkillsComponent {
+
+}
--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { HeroComponent } from '../../features/hero/hero.component';
 import { AboutComponent } from '../../features/about/about.component';
+import { SkillsComponent } from '../../features/skills/skills.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [CommonModule, HeaderComponent, HeroComponent, AboutComponent],
+  imports: [CommonModule, HeaderComponent, HeroComponent, AboutComponent, SkillsComponent],
   templateUrl: './main-layout.component.html',
   styles: `
     :host {
