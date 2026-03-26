c60e10390040ac53a3fb007652bf21c963cc241f
Augusto Cesar <acs-03@hotmail.com>
2026-03-25 22:53:10 -0300

    corrige navegacao dos menus no mobile

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -14,14 +14,25 @@ export class HeaderComponent {
   isMenuOpen = false;
 
   navItems = [
-    { name: 'Home', link: '#' },
-    { name: 'Sobre', link: '#sobre' },
-    { name: 'Habilidades', link: '#habilidades' },
-    { name: 'Projetos', link: '#projetos' },
-    { name: 'Contato', link: '#contato' },
+    { name: 'Home', link: 'home' },
+    { name: 'Sobre', link: 'sobre' },
+    { name: 'Habilidades', link: 'habilidades' },
+    { name: 'Projetos', link: 'projetos' },
+    { name: 'Contato', link: 'contato' },
   ];
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  scrollToSection(sectionId: string) {
+    this.isMenuOpen = false;
+
+    setTimeout(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }, 100);
   }
 }
